8654d32e107d1deeb7af6e5f0d7f0530e68cbf49
小明plus <xiaomingplus@qq.com>
2016-04-28 00:40:03 +0800

    for update

--- libs/check.js
+++ libs/check.js
@@ -393,7 +393,7 @@ check.postCreate = function(o,cb){
 
     var userInfo = check.userInfo({session: o.session,secret:secret});
 
-    if(userInfo.userId == '28874' || userInfo.userId== '28388'){
+    if(userInfo.userId == '28874' || userInfo.userId== '28388' || userInfo.userId =='1'){
       cb(code.userblock);
       return;
     }
